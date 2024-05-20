f321ec25985d448f5f6097b66b40f9a13b1e3cae
Farid Muhammad Eryanto <fbrstudio06@gmail.com>
2024-05-20 11:34:18 +0700

    Add ATM location

--- src/nearest_atm/atm.constant.ts
+++ src/nearest_atm/atm.constant.ts
@@ -58,5 +58,65 @@ export const AtmLocation = [
         address: '707 Kappa Avenue',
         latitude: -6.246890,
         longitude: 106.601120
+    },
+    {
+        name: 'ATM Omega Bank',
+        address: '123 Omega Street',
+        latitude: -6.298800,
+        longitude: 106.791500
+    },
+    {
+        name: 'ATM Sigma Bank',
+        address: '456 Sigma Avenue',
+        latitude: -6.298300,
+        longitude: 106.792400
+    },
+    {
+        name: 'ATM Tau Financial',
+        address: '789 Tau Road',
+        latitude: -6.297800,
+        longitude: 106.791800
+    },
+    {
+        name: 'ATM Upsilon Bank',
+        address: '101 Upsilon Boulevard',
+        latitude: -6.299100,
+        longitude: 106.791300
+    },
+    {
+        name: 'ATM Phi Trust',
+        address: '202 Phi Lane',
+        latitude: -6.298500,
+        longitude: 106.792100
+    },
+    {
+        name: 'ATM Chi Bank',
+        address: '303 Chi Court',
+        latitude: -6.297900,
+        longitude: 106.791200
+    },
+    {
+        name: 'ATM Psi Financial',
+        address: '404 Psi Drive',
+        latitude: -6.298100,
+        longitude: 106.792300
+    },
+    {
+        name: 'ATM Lambda Trust',
+        address: '505 Lambda Parkway',
+        latitude: -6.299000,
+        longitude: 106.792000
+    },
+    {
+        name: 'ATM Nu Bank',
+        address: '606 Nu Street',
+        latitude: -6.298200,
+        longitude: 106.791700
+    },
+    {
+        name: 'ATM Xi Financial',
+        address: '707 Xi Avenue',
+        latitude: -6.297700,
+        longitude: 106.792200
     }
 ];
