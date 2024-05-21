10e1f577f90d2152e7b774d9e6d7f61c97dffa5a
Farid Muhammad Eryanto <fbrstudio06@gmail.com>
2024-05-21 09:13:05 +0700

    Add Docker for Deployment

--- src/nearest_atm/atm.constant.ts
+++ src/nearest_atm/atm.constant.ts
@@ -1,122 +1,137 @@
 export const AtmLocation = [
     {
-        name: 'ATM Alpha Bank',
-        address: '123 Alpha Street',
-        latitude: -6.246144,
-        longitude: 106.601280
+        name: 'ATM Cabang Fatmawati',
+        address: 'Jl. RS. Fatmawati Raya, RT.4/RW.9, Cilandak Bar., Kec. Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12430',
+        latitude: -6.293174708901844,
+        longitude: 106.79512872883495
     },
     {
-        name: 'ATM Beta Bank',
-        address: '456 Beta Avenue',
-        latitude: -6.246920,
-        longitude: 106.599990
-    },
-    {
-        name: 'ATM Gamma Financial',
-        address: '789 Gamma Road',
-        latitude: -6.247321,
-        longitude: 106.600421
-    },
-    {
-        name: 'ATM Delta Bank',
-        address: '101 Delta Boulevard',
-        latitude: -6.247100,
-        longitude: 106.601340
-    },
-    {
-        name: 'ATM Epsilon Trust',
-        address: '202 Epsilon Lane',
-        latitude: -6.246535,
-        longitude: 106.600110
-    },
-    {
-        name: 'ATM Zeta Bank',
-        address: '303 Zeta Court',
-        latitude: -6.247012,
-        longitude: 106.601760
-    },
-    {
-        name: 'ATM Eta Financial',
-        address: '404 Eta Drive',
-        latitude: -6.246800,
-        longitude: 106.599850
-    },
-    {
-        name: 'ATM Theta Trust',
-        address: '505 Theta Parkway',
-        latitude: -6.247433,
-        longitude: 106.600970
-    },
-    {
-        name: 'ATM Iota Bank',
-        address: '606 Iota Street',
-        latitude: -6.246222,
-        longitude: 106.600550
-    },
-    {
-        name: 'ATM Kappa Financial',
-        address: '707 Kappa Avenue',
-        latitude: -6.246890,
-        longitude: 106.601120
-    },
-    {
-        name: 'ATM Omega Bank',
-        address: '123 Omega Street',
-        latitude: -6.298800,
-        longitude: 106.791500
-    },
-    {
-        name: 'ATM Sigma Bank',
-        address: '456 Sigma Avenue',
-        latitude: -6.298300,
-        longitude: 106.792400
-    },
-    {
-        name: 'ATM Tau Financial',
-        address: '789 Tau Road',
-        latitude: -6.297800,
-        longitude: 106.791800
-    },
-    {
-        name: 'ATM Upsilon Bank',
-        address: '101 Upsilon Boulevard',
-        latitude: -6.299100,
-        longitude: 106.791300
-    },
-    {
-        name: 'ATM Phi Trust',
-        address: '202 Phi Lane',
-        latitude: -6.298500,
-        longitude: 106.792100
-    },
-    {
-        name: 'ATM Chi Bank',
-        address: '303 Chi Court',
-        latitude: -6.297900,
-        longitude: 106.791200
-    },
-    {
-        name: 'ATM Psi Financial',
-        address: '404 Psi Drive',
-        latitude: -6.298100,
-        longitude: 106.792300
-    },
-    {
-        name: 'ATM Lambda Trust',
-        address: '505 Lambda Parkway',
-        latitude: -6.299000,
-        longitude: 106.792000
-    },
-    {
-        name: 'ATM Nu Bank',
-        address: '606 Nu Street',
-        latitude: -6.298200,
-        longitude: 106.791700
-    },
-    {
-        name: 'ATM Xi Financial',
-        address: '707 Xi Avenue',
-        latitude: -6.297700,
-        longitude: 106.792200
+        name: 'ATM Cabang Kuningan',
+        address: 'District 8 SCBD, Treasury Tower 26th & 27th Floor, Jl. Jend. Sudirman kav 52-53 Lot 28, RT.5/RW.3, Senayan, Kec. Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12190',
+        latitude: -6.227097833908059,
+        longitude: 106.8063229846602
     }
-];
+]
+
+// export const AtmLocation = [
+//     {
+//         name: 'ATM Alpha Bank',
+//         address: '123 Alpha Street',
+//         latitude: -6.246144,
+//         longitude: 106.601280
+//     },
+//     {
+//         name: 'ATM Beta Bank',
+//         address: '456 Beta Avenue',
+//         latitude: -6.246920,
+//         longitude: 106.599990
+//     },
+//     {
+//         name: 'ATM Gamma Financial',
+//         address: '789 Gamma Road',
+//         latitude: -6.247321,
+//         longitude: 106.600421
+//     },
+//     {
+//         name: 'ATM Delta Bank',
+//         address: '101 Delta Boulevard',
+//         latitude: -6.247100,
+//         longitude: 106.601340
+//     },
+//     {
+//         name: 'ATM Epsilon Trust',
+//         address: '202 Epsilon Lane',
+//         latitude: -6.246535,
+//         longitude: 106.600110
+//     },
+//     {
+//         name: 'ATM Zeta Bank',
+//         address: '303 Zeta Court',
+//         latitude: -6.247012,
+//         longitude: 106.601760
+//     },
+//     {
+//         name: 'ATM Eta Financial',
+//         address: '404 Eta Drive',
+//         latitude: -6.246800,
+//         longitude: 106.599850
+//     },
+//     {
+//         name: 'ATM Theta Trust',
+//         address: '505 Theta Parkway',
+//         latitude: -6.247433,
+//         longitude: 106.600970
+//     },
+//     {
+//         name: 'ATM Iota Bank',
+//         address: '606 Iota Street',
+//         latitude: -6.246222,
+//         longitude: 106.600550
+//     },
+//     {
+//         name: 'ATM Kappa Financial',
+//         address: '707 Kappa Avenue',
+//         latitude: -6.246890,
+//         longitude: 106.601120
+//     },
+//     {
+//         name: 'ATM Omega Bank',
+//         address: '123 Omega Street',
+//         latitude: -6.298800,
+//         longitude: 106.791500
+//     },
+//     {
+//         name: 'ATM Sigma Bank',
+//         address: '456 Sigma Avenue',
+//         latitude: -6.298300,
+//         longitude: 106.792400
+//     },
+//     {
+//         name: 'ATM Tau Financial',
+//         address: '789 Tau Road',
+//         latitude: -6.297800,
+//         longitude: 106.791800
+//     },
+//     {
+//         name: 'ATM Upsilon Bank',
+//         address: '101 Upsilon Boulevard',
+//         latitude: -6.299100,
+//         longitude: 106.791300
+//     },
+//     {
+//         name: 'ATM Phi Trust',
+//         address: '202 Phi Lane',
+//         latitude: -6.298500,
+//         longitude: 106.792100
+//     },
+//     {
+//         name: 'ATM Chi Bank',
+//         address: '303 Chi Court',
+//         latitude: -6.297900,
+//         longitude: 106.791200
+//     },
+//     {
+//         name: 'ATM Psi Financial',
+//         address: '404 Psi Drive',
+//         latitude: -6.298100,
+//         longitude: 106.792300
+//     },
+//     {
+//         name: 'ATM Lambda Trust',
+//         address: '505 Lambda Parkway',
+//         latitude: -6.299000,
+//         longitude: 106.792000
+//     },
+//     {
+//         name: 'ATM Nu Bank',
+//         address: '606 Nu Street',
+//         latitude: -6.298200,
+//         longitude: 106.791700
+//     },
+//     {
+//         name: 'ATM Xi Financial',
+//         address: '707 Xi Avenue',
+//         latitude: -6.297700,
+//         longitude: 106.792200
+//     }
+// ];
